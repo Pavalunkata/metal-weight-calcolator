78479c4a656048130910c884da00f82cd3657f60
Pavel Staykov <pavkata.staikov@gmail.com>
2021-03-31 11:55:43 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,4 +34,6 @@ const [metals, setMetals] = useState(metalsData());
   );
 }
 
+//https://medium.com/@nitinpatel_20236/how-to-build-a-simple-calculator-application-with-react-js-bc10a4568bbd tutorial for calc
+
 export default App;
